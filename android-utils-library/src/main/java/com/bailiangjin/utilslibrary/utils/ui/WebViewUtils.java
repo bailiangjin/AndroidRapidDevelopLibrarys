0b78d9576c569081e11e41f863af8c9f9a02e63b
白良锦 <bailiangjin@bailiangjindeMacBook-Pro-2.local>
2017-04-17 16:57:05 +0800

    更改应用名

--- android-utils-library/src/main/java/com/bailiangjin/utilslibrary/utils/ui/WebViewUtils.java
+++ android-utils-library/src/main/java/com/bailiangjin/utilslibrary/utils/ui/WebViewUtils.java
@@ -19,6 +19,39 @@ public class WebViewUtils {
      * @param url
      */
    public static void load(WebView webView,String url ){
+
+
+//       // 开启 localStorage
+//       WebSettings webSettings=webView.getSettings();
+//      LogUtils.d("OverScrollMode"+webView.getOverScrollMode());
+//
+//       if (Build.VERSION.SDK_INT >= 11){
+//           webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+//       }
+//
+////       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+////           // chromium, enable hardware acceleration
+////           webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+////       } else {
+////           // older android version, disable hardware acceleration
+////           webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+////       }
+////       webSettings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+//
+//       webSettings.setSupportZoom(true);
+//       webSettings.setBuiltInZoomControls(true);
+//       webSettings.setDomStorageEnabled(true);
+//       webSettings.setBlockNetworkImage(false);
+//       // 设置支持javascript
+//       webSettings.setJavaScriptEnabled(true);
+////       webSettings.setEnableSmoothTransition(true);
+//       // 启动缓存
+////       webSettings.setAppCacheEnabled(false);
+//       webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+//       // 设置缓存模式
+////       webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+
+
        // 开启 localStorage
        webView.getSettings().setDomStorageEnabled(true);
        // 设置支持javascript
