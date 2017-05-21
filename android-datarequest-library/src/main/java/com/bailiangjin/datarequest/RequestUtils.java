0362f161c57e87eaef2002def3041dc2b61d0feb
bailiangjin <bailiangjin@qq.com>
2017-05-21 09:41:21 +0800

    抽取配置文件 添加RequestUtils 类

---
+++ android-datarequest-library/src/main/java/com/bailiangjin/datarequest/RequestUtils.java
@@ -0,0 +1,15 @@
+package com.bailiangjin.datarequest;
+
+import com.zhy.http.okhttp.OkHttpUtils;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by bailiangjin on 2017/5/21.
+ */
+
+public class RequestUtils extends OkHttpUtils{
+    public RequestUtils(OkHttpClient okHttpClient) {
+        super(okHttpClient);
+    }
+}
